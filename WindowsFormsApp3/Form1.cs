b85cafd12674abdd4e12eb76ab6075bb03464d3d
woolfik-27 <vladkutas4@gmail.com>
2024-11-15 14:45:28 +0300

    Сделал 1 упр в 1 лаб

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -19,13 +19,28 @@ namespace WindowsFormsApp3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            мротмиомо
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(300, 500);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
         }
     }
 }
